6a869e6f7874ea4b22475670e09d0ffb6f64933d
Mirgorod Alexander <a.mirgorod@open-bs.ru>
2020-09-22 19:13:26 +0300

    Added optional list of numbers for reporting. It must be named as "numbers.list" and must end with an empty line. Each number in it must be on a separate line.

--- dailyreport.py
+++ dailyreport.py
@@ -7,6 +7,17 @@ import push
 
 main_group = sys.argv[1]
 
+personal_numbers = ''
+personal_numbers_list = []
+
+try:
+    with open('numbers.list', 'r', encoding='utf-8') as numbers:
+        for line in numbers:
+            personal_numbers += f"OR dst='{line[:-1]}' "
+            personal_numbers_list.append(line[:-1])
+except FileNotFoundError:
+    print('The file "numbers.list" was not found.')
+
 # connect to sqlite
 try:
     sqlite_conn = sqlite3.connect('state.db')
@@ -31,9 +42,28 @@ sqlite_conn.close()
 support_status = sampling.get_one(calls, main_group)
 common_status = sampling.get_common(calls, main_group)
 answered_statistics = sampling.get_common(calls, main_group, statistics=True)
-if support_status != '':
-    push.post_daily('Missed call statistics for today :meow_party:',
+if support_status:
+    push.post_daily('Unanswered call statistics for today :meow_party:',
                     'Technical support:', support_status,
                     'Common:', common_status,
                     'Statistics:', answered_statistics
+                    )
+
+one = ''
+common = ''
+statistics = ''
+for number in personal_numbers_list:
+    get_one = sampling.get_one(calls, number)
+    get_common = sampling.get_common(calls, number)
+    get_statistics = sampling.get_common(calls, number, statistics=True)
+    if get_one:
+        one += f'\nFor number *{number}*:' + get_one + '\n'
+        common += f'\nFor number *{number}*:' + get_common + '\n'
+        statistics += get_statistics
+
+if one:
+    push.post_daily('Unanswered call statistics by users for today :meow_party:',
+                    'Unanswered:', one,
+                    'Common:', common,
+                    'Statistics:', statistics
                     )
--- mcnotifier.py
+++ mcnotifier.py
@@ -13,6 +13,17 @@ dbpass = sys.argv[3]
 main_group = sys.argv[4]
 next_group = sys.argv[5]
 
+personal_numbers = ''
+personal_numbers_list = []
+
+try:
+    with open('numbers.list', 'r', encoding='utf-8') as numbers:
+        for line in numbers:
+            personal_numbers += f"OR dst='{line[:-1]}' "
+            personal_numbers_list.append(line[:-1])
+except FileNotFoundError:
+    print('The file "numbers.list" was not found.')
+
 # connect to MariaDB
 try:
     mariadb_conn = MySQLdb.connect(
@@ -49,8 +60,8 @@ if not last_stored_id:
 mariadb_cursor.execute(
     f"""SELECT DISTINCT calldate,src,clid,dst,uniqueid,dstchannel,disposition
     FROM cdr
-    WHERE calldate LIKE '{date.today()}%'
-        AND (dst='{main_group}' OR dst='{next_group}')
+    WHERE calldate LIKE '2020-09-09%'
+        AND (dst='{main_group}' {personal_numbers} OR dst='{next_group}')
         AND uniqueid > '{last_stored_id[0]}'
     ORDER BY calldate;"""
 )
@@ -70,8 +81,23 @@ mariadb_conn.close()
 # push statistics to Slack
 support_status = sampling.get_one(calls, main_group)
 common_status = sampling.get_common(calls, main_group)
-if support_status != '':
-    push.post('Missed call statistics :nyancat_big:',
+if support_status:
+    push.post('Unanswered call statistics :nyancat_big:',
+              'Technical support:', support_status,
+              'Common:', common_status
+              )
+
+one = ''
+common = ''
+for number in personal_numbers_list:
+    get_one = sampling.get_one(calls, number)
+    get_common = sampling.get_common(calls, number)
+    if get_one:
+        one += get_one
+        common += get_common
+
+if one:
+    push.post('Unanswered call statistics :nyancat_big:',
               'Technical support:', support_status,
               'Common:', common_status
               )
