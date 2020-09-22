666fc98b9525c5a8d6bcdb80dd296b8ccbcefb0a
Mirgorod Alexander <a.mirgorod@open-bs.ru>
2020-09-22 16:12:08 +0300

    Fixed the sampling.

--- sampling.py
+++ sampling.py
@@ -16,18 +16,24 @@ def get_one(calls_query, calls_group):
     call_status = ''
 
     for (calldate, src, clid, dst, uniqueid, dstchannel, disposition) in calls_query:
+
+        if dstchannel:
+            dstchannel = re.findall(r'/(\d+)', dstchannel)[0]
+        else:
+            dstchannel = '???'
+
         if dst == calls_group:
             if uniqueid == last_uniqueid or last_uniqueid is None:
                 answered_store.append(disposition)
-                dstchannel_store += re.findall(r'/(\d+)', dstchannel)[0] + ' > '
+                dstchannel_store += dstchannel + ' > '
             elif answered_store.count('ANSWERED') == 0:
                 call_status += '\n' + '{} - *{}* is missed call! {} :bangbang:'.format(
                     last_calldate.time(), last_src, dstchannel_store)
                 answered_store = [disposition]
-                dstchannel_store = re.findall(r'/(\d+)', dstchannel)[0] + ' > '
+                dstchannel_store = dstchannel + ' > '
             else:
                 answered_store = [disposition]
-                dstchannel_store = re.findall(r'/(\d+)', dstchannel)[0] + ' > '
+                dstchannel_store = dstchannel + ' > '
 
             last_calldate = datetime.strptime(str(calldate), '%Y-%m-%d %H:%M:%S')
             last_src = src
@@ -52,33 +58,47 @@ def get_common(calls_query, first_group, statistics=False):
     dstchannel_store = ''
 
     call_status = ''
+    hero = ''
     heroes = []
     answered_statistics = ''
 
     for (calldate, src, clid, dst, uniqueid, dstchannel, disposition) in calls_query:
+
+        if dstchannel:
+            dstchannel = re.findall(r'/(\d+)', dstchannel)[0]
+        else:
+            dstchannel = '???'
+
         if uniqueid == last_uniqueid or last_uniqueid is None:
             answered_store.append(disposition)
             dst_store.append(dst)
-            dstchannel_store += re.findall(r'/(\d+)', dstchannel)[0] + ' > '
+            dstchannel_store += dstchannel + ' > '
         elif answered_store.count('ANSWERED') == 0 and dst_store.count(first_group) > 0:
             call_status += '\n' + '{} - *{}* is missed call! {} :bangbang:'.format(
                 last_calldate.time(), last_src, dstchannel_store)
             answered_store = [disposition]
             dst_store = [dst]
-            dstchannel_store = re.findall(r'/(\d+)', dstchannel)[0] + ' > '
-        elif dst_store.count(first_group) > 0:
+            dstchannel_store = dstchannel + ' > '
+        elif answered_store.count('ANSWERED') > 0 and dst_store.count(first_group) > 0:
             call_status += '\n' + '{} - *{}* was answered by *{}*! {} :heavy_check_mark:'.format(
                 last_calldate.time(), last_src,
-                re.findall(r'/(\d+)', last_dstchannel)[0], dstchannel_store)
+                hero, dstchannel_store)
             answered_store = [disposition]
             dst_store = [dst]
-            heroes.extend([re.findall(r'/(\d+)', last_dstchannel)[0]])
-            dstchannel_store = re.findall(r'/(\d+)', dstchannel)[0] + ' > '
+            heroes.extend([last_dstchannel])
+            dstchannel_store = dstchannel + ' > '
+        else:
+            answered_store = [disposition]
+            dst_store = [dst]
+            dstchannel_store = dstchannel + ' > '
 
         last_calldate = datetime.strptime(str(calldate), '%Y-%m-%d %H:%M:%S')
         last_src = src
         last_uniqueid = uniqueid
         last_dstchannel = dstchannel
+
+        if disposition == 'ANSWERED':
+            hero = dstchannel
 
     if last_uniqueid is not None and answered_store.count('ANSWERED') == 0 and dst_store.count(first_group) > 0:
         call_status += '\n' + '{} - *{}* is missed call! {} :bangbang:'.format(
@@ -86,8 +106,8 @@ def get_common(calls_query, first_group, statistics=False):
     elif dst_store.count(first_group) > 0:
         call_status += '\n' + '{} - *{}* was answered by *{}*! {} :heavy_check_mark:'.format(
             last_calldate.time(), last_src,
-            re.findall(r'/(\d+)', last_dstchannel)[0], dstchannel_store)
-        heroes.extend([re.findall(r'/(\d+)', last_dstchannel)[0]])
+            hero, dstchannel_store)
+        heroes.extend([last_dstchannel])
 
     answered_statistics_store = dict((i, heroes.count(i)) for i in heroes)
     for i in answered_statistics_store:
